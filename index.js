18db084d47d1f8d57d331a6f4087df620a2c9339
zyttor <zyttor@gmail.com>
2021-01-02 00:18:19 -0600

    seguro

--- index.js
+++ index.js
@@ -16,13 +16,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
-//const CONNECTION_URL = 'mongodb+srv://Admin01:juan111213@cluster0.uynfo.mongodb.net/<dbname>?retryWrites=true&w=majority';
-
 
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`server run on port: ${PORT}`)))
     .catch((err) => console.log(err.message));
+
 
 mongoose.set('useFindAndModify', false);
